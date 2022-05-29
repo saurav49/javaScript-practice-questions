8f021e1c97819171dbc5fd62b8cbe1630d5b9fe6
Saurav Biswas <38159915+saurav49@users.noreply.github.com>
2022-05-29 13:02:02 +0530

    chore

--- practice-1.js
+++ practice-1.js
@@ -56,3 +56,42 @@ function infiniteCurr(a) {
 }
 
 console.log(infiniteCurr(1)(2)(3)(4)());
+
+function isAnagram(w1, w2) {
+  if (w1.length !== w2.length) {
+    return false;
+  }
+
+  let w1Obj = {};
+  let w2Obj = {};
+
+  for (let letter of w1) {
+    if (w1Obj[letter]) {
+      w1Obj[letter] += 1; // o(n)
+    } else {
+      w1Obj[letter] = 1;
+    }
+  }
+
+  for (let letter of w2) {
+    if (w2Obj[letter]) {
+      w2Obj[letter] += 1; // o(n)
+    } else {
+      w2Obj[letter] = 1;
+    }
+  }
+
+  for (let key in w1Obj) {
+    // o(n)
+    if (!w2Obj[key]) {
+      return false;
+    }
+
+    if (w2Obj[key] && w2Obj[key] !== w1Obj[key]) return false;
+  }
+
+  return true; // o(n)
+}
+
+console.log(isAnagram("below", "elbow"));
+// silent === tne
