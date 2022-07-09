8a2b80b037d1cc9a01cb3356be037e3f1da79562
Saurav Biswas <38159915+saurav49@users.noreply.github.com>
2022-07-09 13:01:37 +0530

    refractor

--- callApplyBind.js
+++ callApplyBind.js
@@ -1,44 +1,86 @@
-let nameObj = {
-  firstname: "saurav",
-  lastname: "biswas",
-};
-
-const printFullName = function (hometown, state) {
-  console.log(`${this.firstname} ${this.lastname} from ${hometown} ${state}`);
-};
-
-// printFullName.call(nameObj, "Silchar", "Assam");
-// let name2 = {
-//   firstname: "Sachin",
-//   lastname: "Tendulkar",
+// let nameObj = {
+//   firstname: "saurav",
+//   lastname: "biswas",
 // };
 
-// FUNCTION BORROWING
-// printFullName.call(name2, "Mumbai", "Maharastra");
+// const printFullName = function (hometown, state) {
+//   console.log(`${this.firstname} ${this.lastname} from ${hometown} ${state}`);
+// };
 
-//APPLY
-// printFullName.apply(name2, ["Mumbai", "Maharastra"]);
+// // printFullName.call(nameObj, "Silchar", "Assam");
+// // let name2 = {
+// //   firstname: "Sachin",
+// //   lastname: "Tendulkar",
+// // };
 
-//BIND
-let getFullName = printFullName.bind(nameObj, "Silchar");
-getFullName("Assam");
+// // FUNCTION BORROWING
+// // printFullName.call(name2, "Mumbai", "Maharastra");
 
-var bindGetter = printFullName.bind(nameObj, "arg");
-bindGetter("arg1", "arg2");
-//My__BIND
-Function.prototype.myBind = function (scope, ...arg) {
-  let fn = this;
-  return function (...arg1) {
-    fn.call(scope, ...arg, ...arg1);
-  };
-};
-// Function.prototype.mybind = function (...args) {
-//   let obj = this;
-//   const params = args.slice(1);
-//   return function (...args1) {
-//     obj.apply(args[0], [...params, ...args1]);
+// //APPLY
+// // printFullName.apply(name2, ["Mumbai", "Maharastra"]);
+
+// //BIND
+// let getFullName = printFullName.bind(nameObj, "Silchar");
+// getFullName("Assam");
+
+// var bindGetter = printFullName.bind(nameObj, "arg");
+// bindGetter("arg1", "arg2");
+// //My__BIND
+// Function.prototype.myBind = function (scope, ...arg) {
+//   let fn = this;
+//   return function (...arg1) {
+//     fn.call(scope, ...arg, ...arg1);
 //   };
 // };
+// // Function.prototype.mybind = function (...args) {
+// //   let obj = this;
+// //   const params = args.slice(1);
+// //   return function (...args1) {
+// //     obj.apply(args[0], [...params, ...args1]);
+// //   };
+// // };
 
-let getmyBindFullName = printFullName.myBind(nameObj, "Silchar", "Assam");
-getmyBindFullName();
+// let getmyBindFullName = printFullName.myBind(nameObj, "Silchar", "Assam");
+// getmyBindFullName();
+
+let name1 = {
+  firstName: "Akshay",
+  lastName: "Saini",
+};
+
+let name2 = {
+  firstName: "Sachin",
+  lastName: "Tendulkar",
+};
+
+function printFullName(homeTown, state) {
+  console.log(`${this.firstName} ${this.lastName} from ${homeTown} ${state}`);
+}
+
+Function.prototype.myCall = function (...args) {
+  let fn = this;
+  let obj = args[0];
+  obj._ = fn;
+
+  obj._(...args.slice(1));
+};
+
+Function.prototype.myApply = function (obj, args) {
+  let fn = this;
+  obj._ = fn;
+
+  obj._(...args);
+};
+
+Function.prototype.myBind = function (obj, ...args) {
+  let fn = this;
+  obj._ = fn;
+  return (...args1) => {
+    obj._(...args, ...args1);
+  };
+};
+
+printFullName.myCall(name1, "new delhi", "delhi");
+printFullName.myApply(name2, ["mumbai", "maharashtra"]);
+const caller = printFullName.myBind(name1, "ghy");
+caller("assam1");
